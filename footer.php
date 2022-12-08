5e9f8df27e9ed6e4264c91f6f9f495e65834a9b5
Luis Cabrera Benito <parzibyte@gmail.com>
2022-12-08 13:02:51 -0600

    Footer y cambios menores

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="hero is-link">
+    <div class="hero-body">
+
+        <div class="columns">
+            <div class="column">
+                <strong>Conéctate conmigo (no responderé mensajes, solo puedes seguirme)</strong>
+                <ul>
+                    <li><a href="https://github.com/parzibyte">GitHub</a></li>
+                    <li><a href="https://facebook.com/parzibyte.fanpage">Facebook</a></li>
+                    <li><a href="https://twitter.com/parzibyte">Twitter</a></li>
+                    <li><a href="https://www.youtube.com/@parzibyte">YouTube</a></li>
+                    <li><a href="https://www.instagram.com/parzibyte/">Instagram</a></li>
+                    <li><a href="https://stackoverflow.com/users/5032550/luis-cabrera-benito">Stack Overflow</a></li>
+                </ul>
+            </div>
+            <div class="column">
+                <strong>Menú</strong>
+                <ul>
+                    <li> <a href="#">Inicio</a> </li>
+                    <li> <a href="#servicios">Servicios</a> </li>
+                    <li> <a href="#portafolio">Portafolio</a> </li>
+                    <li> <a href="#contacto">Contacto</a> </li>
+                </ul>
+            </div>
+            <div class="column">
+                <strong>Más</strong>
+                <ul>
+                    <li>
+                        <a href="https://parzibyte.me/blog/">Blog</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <h3 class="is-size-3">Parzibyte.me</h3>
+                <p>&copy; 2017-<?php echo date("Y"); ?> Parzibyte. Todos los derechos reservados.</p>
+            </div>
+        </div>
+    </div>
+</section>
--- inicio.php
+++ inicio.php
@@ -1,22 +1,22 @@
 <section class="hero is-link">
-        <div class="hero-body">
-            <div class="columns">
-                <div class="column is-two-thirds">
-                    <p class="title">
-                        <?php echo traducir("Estoy aquí para ayudarte, brindando mis servicios de consultoría informática"); ?>
-                    </p>
-                    <p class="subtitle">
-                        <?php echo traducir("Hola, soy Parzibyte. Puedo ayudar con tu tarea de programación, desarrollar cualquier software web o móvil, configurar servidores y todo lo relacionado con programación, TI e informática"); ?>
-                    </p>
-                    <div class="buttons">
-                        <button class="button is-info is-rounded"><?php echo traducir("Servicios"); ?></button>
-                        <button class="button is-success is-rounded"><?php echo traducir("Contacto"); ?></button>
-                        <button class="button is-warning is-rounded"><?php echo traducir("Blog"); ?></button>
-                    </div>
-                </div>
-                <div class="column">
-                    <img src="./images/freelance-developer.svg" alt="">
+    <div class="hero-body">
+        <div class="columns">
+            <div class="column is-two-thirds">
+                <p class="title">
+                    <?php echo traducir("Estoy aquí para ayudarte, brindando mis servicios de consultoría informática"); ?>
+                </p>
+                <p class="subtitle">
+                    <?php echo traducir("Hola, soy Parzibyte. Puedo ayudar con tu tarea de programación, desarrollar cualquier software web o móvil, configurar servidores y todo lo relacionado con programación, TI e informática"); ?>
+                </p>
+                <div class="buttons">
+                    <a class="button is-info is-rounded" href="#servicios"><?php echo traducir("Servicios"); ?></a>
+                    <a class="button is-success is-rounded" href="#contacto"><?php echo traducir("Contacto"); ?></a>
+                    <a class="button is-warning is-rounded" target="blank" href="https://parzibyte.me/blog/"><?php echo traducir("Blog"); ?></a>
                 </div>
             </div>
+            <div class="column">
+                <img src="./images/freelance-developer.svg" alt="">
+            </div>
         </div>
-    </section>
+    </div>
+</section>
